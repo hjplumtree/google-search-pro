4bbd209b084724ac796e90645d850592b6d0ac5c
hej <dark199174@gmail.com>
2022-01-14 16:26:37 +0900

    Add Image search

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ function SearchContainer(props) {
   return (
     <section>
       {/* <SearchDefault />
-      <SearchInSite /> */}
-      <SearchFileType />
+      <SearchInSite />
+      <SearchFileType /> */}
+      <SearchImage />
     </section>
   );
 }
@@ -98,6 +99,51 @@ function SearchFileType() {
             onChange={(evt) => setFileType(evt.target.value)}
           />
           <p>Types: pdf, ps, hwp, html, xls, ppt, doc, svg ...</p>
+        </label>
+      </p>
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
+
+function SearchImage() {
+  const [query, setQuery] = useState("");
+  const [width, setWidth] = useState("");
+  const [height, setHeight] = useState("");
+  function submitHandler(evt) {
+    evt.preventDefault();
+    window.open(
+      `https://www.google.com/search?q=${query}+imagesize%3A${width}x${height}&tbm=isch`,
+    );
+  }
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="query">
+        Search
+        <input
+          name="query"
+          type="text"
+          onChange={(evt) => setQuery(evt.target.value)}
+        />
+      </label>
+      <p>
+        <label htmlFor="file-type">
+          Width
+          <input
+            name="file-type"
+            type="number"
+            onChange={(evt) => setWidth(evt.target.value)}
+          />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="file-type">
+          Height
+          <input
+            name="file-type"
+            type="number"
+            onChange={(evt) => setHeight(evt.target.value)}
+          />
         </label>
       </p>
       <input type="submit" value="Search" />
