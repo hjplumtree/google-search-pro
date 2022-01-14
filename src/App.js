3543a802e228e9d3b7b2a461259086a377d95d33
hej <dark199174@gmail.com>
2022-01-14 16:03:57 +0900

    Add site search sketch

--- src/App.js
+++ src/App.js
@@ -10,13 +10,21 @@ function App() {
 }
 
 function SearchContainer(props) {
+  return (
+    <section>
+      {/* <SearchDefault /> */}
+      <SearchInSite />
+    </section>
+  );
+}
+
+function SearchDefault() {
   const [inputValue, setInputValue] = useState("");
 
   function submitHandler(evt) {
     evt.preventDefault();
     window.open(`http://google.com/search?q=${inputValue}`);
   }
-
   return (
     <form onSubmit={submitHandler}>
       <input
@@ -29,4 +37,38 @@ function SearchContainer(props) {
   );
 }
 
+function SearchInSite() {
+  const [query, setQuery] = useState("");
+  const [siteUrl, setSiteUrl] = useState("");
+  function submitHandler(evt) {
+    evt.preventDefault();
+    window.open(`https://www.google.com/search?q=site%3A${siteUrl}+${query}`);
+  }
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="query">
+        Search
+        <input
+          name="query"
+          type="text"
+          onChange={(evt) => setQuery(evt.target.value)}
+        />
+      </label>
+      <p>
+        <label htmlFor="site-url">
+          Site Url
+          <input
+            name="site-url"
+            type="text"
+            onChange={(evt) => setSiteUrl(evt.target.value)}
+          />
+        </label>
+      </p>
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
+
 export default App;
+
+// https://www.google.com/search?q=site%3Anaver.com+%EA%B6%81%EA%B8%88%ED%95%B4
