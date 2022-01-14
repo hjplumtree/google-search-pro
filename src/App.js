a22e283eeb7fde0af851fd2d5436b3bbf841b15b
hej <dark199174@gmail.com>
2022-01-14 16:26:01 +0900

    Add search file type

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ function App() {
 function SearchContainer(props) {
   return (
     <section>
-      {/* <SearchDefault /> */}
-      <SearchInSite />
+      {/* <SearchDefault />
+      <SearchInSite /> */}
+      <SearchFileType />
     </section>
   );
 }
@@ -62,6 +63,41 @@ function SearchInSite() {
             type="text"
             onChange={(evt) => setSiteUrl(evt.target.value)}
           />
+        </label>
+      </p>
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
+
+function SearchFileType() {
+  const [query, setQuery] = useState("");
+  const [fileType, setFileType] = useState("");
+  function submitHandler(evt) {
+    evt.preventDefault();
+    window.open(
+      `https://www.google.com/search?q=filetype%3A${fileType}+${query}`,
+    );
+  }
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="query">
+        Search
+        <input
+          name="query"
+          type="text"
+          onChange={(evt) => setQuery(evt.target.value)}
+        />
+      </label>
+      <p>
+        <label htmlFor="file-type">
+          File Type
+          <input
+            name="file-type"
+            type="text"
+            onChange={(evt) => setFileType(evt.target.value)}
+          />
+          <p>Types: pdf, ps, hwp, html, xls, ppt, doc, svg ...</p>
         </label>
       </p>
       <input type="submit" value="Search" />
