88d1913df73319e21ec8ff6c8d55ddc50e62d9ef
hej <dark199174@gmail.com>
2022-01-14 18:26:15 +0900

    Add keyword Search

--- src/App.js
+++ src/App.js
@@ -12,10 +12,11 @@ function App() {
 function SearchContainer(props) {
   return (
     <section>
-      {/* <SearchDefault />
-      <SearchInSite />
-      <SearchFileType /> */}
-      <SearchImage />
+      <SearchKeywords />
+      {/* <SearchDefault /> */}
+      {/* <SearchInSite /> */}
+      {/* <SearchFileType /> */}
+      {/* <SearchImage /> */}
     </section>
   );
 }
@@ -33,6 +34,77 @@ function SearchDefault() {
         name="search"
         type="text"
         onChange={(evt) => setInputValue(evt.target.value)}
+      />
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
+
+function SearchKeywords() {
+  const [queries, setQueries] = useState([{ id: 1, q: "" }]);
+
+  function submitHandler(evt) {
+    evt.preventDefault();
+
+    let query = "";
+    if (queries.length > 1) {
+      query = queries
+        .reduce((acc, cur) => {
+          return acc + `%2B"${cur.q}"`;
+        }, "")
+        .slice(3);
+    } else {
+      query = `"${queries[0].q}"`;
+    }
+    console.log(query);
+    window.open(`https://www.google.com/search?q=${query}`);
+  }
+
+  function handleChange(evt) {
+    const { value } = evt.target;
+    const name = Number(evt.target.name);
+    const newQueries = [...queries];
+    for (let i = 0; i < newQueries.length; i++) {
+      const query = newQueries[i];
+      if (query.id === name) {
+        query.q = value;
+      }
+    }
+    setQueries(newQueries);
+  }
+
+  function handleClick(evt) {
+    evt.preventDefault();
+    const newId = queries.length + 1;
+    const newQueries = [...queries, { id: newId, q: "", delete: true }];
+    setQueries(newQueries);
+  }
+
+  function handleDelete(id) {
+    const newQueries = queries.filter((el) => el.id !== id);
+    setQueries(newQueries);
+  }
+  return (
+    <form onSubmit={submitHandler}>
+      {queries.map((el) => {
+        return (
+          <p key={el.id}>
+            <label>
+              Keyword Search
+              <input name={el.id} type="text" onChange={handleChange} />
+            </label>
+            {el.delete && (
+              <button onClick={() => handleDelete(el.id)}>X</button>
+            )}
+          </p>
+        );
+      })}
+
+      <input
+        disabled={queries.length === 2 && true}
+        type="button"
+        value="Add Keyword"
+        onClick={handleClick}
       />
       <input type="submit" value="Search" />
     </form>
@@ -152,5 +224,3 @@ function SearchImage() {
 }
 
 export default App;
-
-// https://www.google.com/search?q=site%3Anaver.com+%EA%B6%81%EA%B8%88%ED%95%B4
