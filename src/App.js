9165a62cc4939847db13ebd384ee902a2446b797
hej <dark199174@gmail.com>
2022-01-24 10:49:31 +0900

    Add Header style

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import Logo from "./components/Logo";
 import SearchContainer from "./components/SearchContainer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
         <Logo />
+        <Header />
         <SearchContainer />
       </BrowserRouter>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+﻿import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Easiest way to Google better</h1>
+      <p>You know there are great tips.</p>
+      <p>Here you are! But way more friendly to use 😉</p>
+    </header>
+  );
+}
